6a824e31adef96bf2a422d7f30afc73c67ebfaff
Brian Kubes <briankubes@gmail.com>
2020-11-19 14:31:06 -0800

    added test, about to move the folder to arts-routes

--- api/server-integration.test.js
+++ api/server-integration.test.js
@@ -1,98 +1,162 @@
-const db = require('../data/dbConfig.js')
-const request = require('supertest')
-const server = require('./server.js')
+const db = require("../data/dbConfig.js");
+const request = require("supertest");
+const server = require("./server.js");
 
+describe("test restricted arts-router", () => {
+  const registerUser = {
+    email: "me@me.com",
+    password: "123",
+  };
+  const loginUser = {
+    email: "me@me.com",
+    password: "123",
+  };
+  let token;
+  beforeEach(async () => {
+    await db("users").truncate();
+    request(server)
+      .post("/api/auth/register")
+      .send(registerUser)
+      .then((res) => {
+        console.log("register response -->", res.body);
+      });
+  });
+  beforeEach((done) => {
+    request(server)
+      .post("/api/auth/login")
+      .send(loginUser)
+      .then((res) => {
+        token = res.body.token;
+        console.log("the response you're looking for -->", token);
+        done();
+      });
+  });
+  afterEach(async () => {
+    await db("users").truncate();
+  });
+  beforeEach(async () => {
+    await db("arts").truncate();
+  });
 
-describe('test restricted arts-router', () => {
-
-    const registerUser = {
-        email: 'me@me.com',
-        password: '123'
-    }
-    const loginUser = {
-        email: 'me@me.com',
-        password: '123'
-    }
-    let token;
-    beforeEach(async ()=> {
-        await db('users').truncate()
-        request(server)
-        .post('/api/auth/register')
-        .send(registerUser)
-        .then(res => {
-            console.log("register response -->", res.body)
-        })
-    })
-    beforeEach((done) => {
-        request(server)
-        .post('/api/auth/login')
-        .send(loginUser)
-        .then((res) => {
-            token = res.body.token
-            console.log("the response you're looking for -->", token)
-            done();
-        });
+  describe("private route [GET]", () => {
+    it("returns 404", () => {
+      return request(server)
+        .get("/api/arts/1")
+        .set("Authorization", token)
+        .expect(404);
     });
-    afterEach(async () => {
-        await db('users').truncate()
-    })
-    beforeEach(async ()=> {
-        await db('arts').truncate()
-    })
+    it("add art, should return 200", async () => {
+      await db("arts").insert({
+        art_name: "test",
+        art_url: "test",
+        rating: 5,
+        category: "news",
+        users_id: 1,
+      });
+      return request(server)
+        .get("/api/arts/1")
+        .set("Authorization", token)
+        .expect(200);
+    });
+  });
 
-   describe('private route [GET]', () => {
-       it ('returns 404', () => {
-           return request(server).get('/api/arts/1').set('Authorization', token)
-           .expect(404)   
-       })
-       it ('add art, should return 200', async () => {
-           await db('arts').insert({ art_name: "test", art_url: "test", rating: 5, category: "news", users_id: 1 })
-           return request(server).get('/api/arts/1').set('Authorization', token)
-           .expect(200)   
+  describe("private route [POST]", () => {
+    it("add art, returns 201", () => {
+      return request(server)
+        .post("/api/arts/1")
+        .send({
+          art_name: "test",
+          art_url: "test",
+          rating: 5,
+          category: "news",
         })
+        .set("Authorization", token)
+        .expect(201);
+    });
+  });
 
-   })
- 
-   describe('private route [POST]', () => {
-    it ('add art, returns 201', () => {
-        return request(server).post('/api/arts/1').send({ art_name: "test", art_url: "test", rating: 5, category: "news" }).set('Authorization', token)
-        .expect(201)     
-    })
-    })
+  describe("private route category [GET]", () => {
+    it("no arts returns 404", async () => {
+      return request(server)
+        .get("/api/arts/category/1/music")
+        .set("Authorization", token)
+        .expect(404);
+    });
 
-    describe('private route category [GET]', () => {
-        it ('no arts returns 404', async () => {
-            return request(server).get('/api/arts/category/1/music').set('Authorization', token)
-            .expect(404)
-        })
+    it("add arts and returns 200", async () => {
+      await db("arts").insert({
+        art_name: "test",
+        art_url: "test",
+        rating: 5,
+        category: "music",
+        users_id: 1,
+      });
+      await db("arts").insert({
+        art_name: "test2",
+        art_url: "test2",
+        rating: 4,
+        category: "music",
+        users_id: 1,
+      });
+      await db("arts").insert({
+        art_name: "test3",
+        art_url: "test3",
+        rating: 3,
+        category: "news",
+        users_id: 1,
+      });
+      return request(server)
+        .get("/api/arts/category/1/music")
+        .set("Authorization", token)
+        .expect(200);
+    });
+  });
 
-        it ('add arts and returns 200', async () => {
-            await db('arts').insert({ art_name: "test", art_url: "test", rating: 5, category: "music", users_id: 1 })
-            await db('arts').insert({ art_name: "test2", art_url: "test2", rating: 4, category: "music", users_id: 1 })
-            await db('arts').insert({ art_name: "test3", art_url: "test3", rating: 3, category: "news", users_id: 1 })
-            return request(server).get('/api/arts/category/1/music').set('Authorization', token)
-            .expect(200)
-        })
-    } )
+  describe("restricted route [PUT]", () => {
+    it("add art, update with bad object and expect 500", async () => {
+      await db("arts").insert({
+        art_name: "test",
+        art_url: "test",
+        rating: 5,
+        category: "music",
+        users_id: 1,
+      });
+      return request(server)
+        .put("/api/arts/1")
+        .send({})
+        .set("Authorization", token)
+        .expect(500);
+    });
+    it("add art, update it and expect 200", async () => {
+      await db("arts").insert({
+        art_name: "test",
+        art_url: "test",
+        rating: 5,
+        category: "music",
+        users_id: 1,
+      });
+      return request(server)
+        .put("/api/arts/1")
+        .send({ art_name: "change" })
+        .set("Authorization", token)
+        .expect(200);
+    });
+  });
 
-    describe('restricted route [PUT]', () => {
-        it ('add art, update with bad object and expect 500', async () => {
-            await db('arts').insert({ art_name: "test", art_url: "test", rating: 5, category: "music", users_id: 1 })
-            return request(server).put('/api/arts/1').send({ }).set('Authorization', token)
-            .expect(500)
-        })
-        it ('add art, update it and expect 200', async () => {
-            await db('arts').insert({ art_name: "test", art_url: "test", rating: 5, category: "music", users_id: 1 })
-            return request(server).put('/api/arts/1').send({ art_name: "change" }).set('Authorization', token)
-            .expect(200)
-        })
-    })
-
-    
-
-
-})
-
-
-
-
+  describe("restricted route [DELETE]", () => {
+    it("add art, delete that art, return shoulb be 200", async () => {
+      await db("arts").insert({
+        art_name: "test",
+        art_url: "test",
+        rating: 5,
+        category: "music",
+        users_id: 1,
+      });
+      return request(server)
+        .delete("/api/arts/1")
+        .set("Authorization", token)
+        .expect(200)
+        .expect("Content-Type", /json/);
+    });
+  });
+});
--- api/server.test.js
+++ api/server.test.js
@@ -1,41 +1,26 @@
-const server = require('./server.js')
-const request = require('supertest')
-const db = require('../data/dbConfig.js')
+const server = require("./server.js");
+const request = require("supertest");
+const db = require("../data/dbConfig.js");
 
+describe("GET", () => {
+  beforeEach(async () => {
+    await db("arts").truncate();
+  });
 
-describe('GET', () => {
-    beforeEach(async () => {
-        await db('arts').truncate()
-    })
+  it('has process.env.DB_ENV as "testing"', () => {
+    expect(process.env.DB_ENV).toBe("testing");
+  });
 
-    it ('has process.env.DB_ENV as "testing"', () => {
-        expect(process.env.DB_ENV).toBe('testing')
-    })
+  it('has process.env.DB_ENV not to be "development', () => {
+    expect(process.env.DB_ENV).not.toBe("development");
+  });
 
-    it ('has process.env.DB_ENV not to be "development', () => {
-        expect(process.env.DB_ENV).not.toBe('development')
-    })
-
-    it ('returns 200 OK', () => {
-        return request(server).get('/')
-        .expect(200)
-        .expect('Content-Type', /text/)
-        .expect('Content-Length', '20')
-        // .expect('Content-Type', /json/)
-
-    })
-
-    // it ('returns 404', () => {
-    //     return request(server).get('/api/arts/1')
-    //     .expect(404)
-        
-    // })
-
-    // it ('returns 200', () => {
-    //     return request(server).get('/api/arts/1')
-    //     .expect('Content-Type', /json/)
-    //     .expect(200)   
-    // })
-
-    
-})
+  it("returns 200 OK", () => {
+    return request(server)
+      .get("/")
+      .expect(200)
+      .expect("Content-Type", /text/)
+      .expect("Content-Length", "20");
+    // .expect('Content-Type', /json/)
+  });
+});
